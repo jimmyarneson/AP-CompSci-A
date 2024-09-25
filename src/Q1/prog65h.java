8aeffe9938ea62bb09d4568e35fd17cc875e69b3
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-09-25 16:01:06 +0000

    update

---
+++ src/Q1/prog65h.java
@@ -0,0 +1,30 @@
+package Q1;
+import java.util.*;
+
+public class prog65h {
+    public static void main(String[] args) {
+        Scanner numbers = new Scanner(System.in);
+
+        System.out.println("Enter pounds: ");
+        double pounds = numbers.nextDouble();
+
+        System.out.println("Enter Shillings");
+        double shillings = numbers.nextDouble();
+
+        System.out.println("Enter pence: ");
+        double pence = numbers.nextDouble();
+
+
+        double decpounds = pounds + shillings/20 + pence/12/20;
+
+        System.out.printf("Decimal pounds: %.2f\n", decpounds);
+
+        // pound = 2.0 shillings
+        // shilling 12 pence 0.12
+        // pence = 0.012
+    }
+
+}
+/?
+
+/?
