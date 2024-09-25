349648c565b4ae212643033000a4c9f998872212
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-09-25 18:43:19 +0000

    change

--- src/Q1/prog72t.java
+++ src/Q1/prog72t.java
@@ -11,6 +11,14 @@ public class prog72t {
         System.out.print("Enter time 2: ");
         int time2 = time.nextInt();
 
+        int hourspast = time2 - time1;
+
+        System.out.println("Hours Past: " + hourspast);
 
     }
 }
+/*
+ Enter time 1: 900
+ Enter time 2: 1730
+ Hours Past: 830
+ */
