1aa85940d77fd578efe89b90136b6e399fb9aa4a
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-09-26 11:23:23 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ src/Q1/prog122a.java
@@ -0,0 +1,13 @@
+package Q1;
+import java.util.*;
+
+public class prog122a {
+    public static void main(String[] args) {
+        int lcv = 0;
+        while (lcv < 50) {
+            lcv += 1.0;
+            System.out.println(lcv);
+        }
+
+    }
+}
