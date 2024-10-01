0812669073e7a51aad911c188a1f3fa75a6a7558
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-10-01 11:25:38 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ src/Q1/lang122c.java
@@ -0,0 +1,15 @@
+package Q1;
+
+public class lang122c {
+    public static void main(String[] args) {
+
+        int lcv = 0;
+        int lcv2 = 0
+        while (lcv < 10) {
+            lcv += 2.0;
+
+            System.out.println(lcv);
+
+        }
+    }
+}
---
+++ src/Q1/strInt6.java
@@ -0,0 +1,16 @@
+package Q1;
+import java.util.Scanner;
+public class strInt6 {
+    public static void main(String[] args) {
+        Scanner wow = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = wow.nextLine().toLowerCase().trim();
+
+        
+
+        for (int lcv = 0; lcv < text.length(); lcv++) {
+
+        }
+    }
+}
