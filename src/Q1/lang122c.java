143d1165933d583b8d43679dd89e7e59e8c5cb89
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-10-02 11:35:30 -0500

    Merge remote-tracking branch 'origin/master'

--- src/Q1/lang122c.java
+++ src/Q1/lang122c.java
@@ -4,12 +4,25 @@ public class lang122c {
     public static void main(String[] args) {
 
         int lcv = 0;
-        int lcv2 = 0
+
         while (lcv < 10) {
             lcv += 2.0;
+            int lcv2 = 0;
+            lcv2 += lcv +1.0;
+            int lcv3 = 0;
+            lcv3 += lcv * 2;
+            int lcv4 = 0;
+            lcv4 += Math.pow(lcv,2);
 
-            System.out.println(lcv);
+            System.out.println(lcv + "\t\t" + lcv2 + "\t\t" + lcv3 + "\t\t" + lcv4);
 
         }
     }
 }
+/*
+2		3		4		4
+4		5		8		16
+6		7		12		36
+8		9		16		64
+10		11		20		100
+ */
---
+++ src/Q1/prog122d.java
@@ -0,0 +1,12 @@
+package Q1;
+
+public class prog122d {
+    public static void main(String[] args) {
+        int lcv = -13;
+        while (lcv < 16){
+            lcv += 1;
+            double y = Math.pow(lcv, 6) - Math.pow(lcv, 5)
+            System.out.println(lcv);
+        }
+    }
+}
