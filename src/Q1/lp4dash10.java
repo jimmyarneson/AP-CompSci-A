c3f4aecee002f2f46600a764dbf711ccc385cb3c
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-10-16 19:54:23 +0000

    update

--- src/Q1/lp4dash10.java
+++ src/Q1/lp4dash10.java
@@ -23,10 +23,42 @@ public class lp4dash10 {
 
         System.out.println("Sphere");
 
-        System.out.print("Enter the radius");
+        System.out.print("Enter the radius: ");
         double radius = input.nextDouble();
         System.out.println();
 
-        double volume = 3.14
+        double d = 2*radius;
+        double volume = 3.14 * d * d * d / 6;
+        System.out.printf("The volume of the sphere is: " + volume);
+        System.out.println();
+
+        System.out.print("Enter length for all four sides: ");
+        int length2 = input.nextInt();
+        System.out.println();
+
+        System.out.println("Cube");
+
+
+        double volume2 = Math.pow(length2, 3);
+
+        System.out.println("The volume is: "+ volume2);
     }
 }
+/*
+ Rectangular Prism
+Enter length: 3
+
+Enter width: 4
+
+Enter height: 5
+
+The volume is: 60
+Sphere
+Enter the radius: 3
+
+The volume of the sphere is: 113.04
+Enter length for all four sides: 4
+
+Cube
+The volume is: 64.0
+ */
