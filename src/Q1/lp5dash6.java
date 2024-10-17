14b00b728bcac9ed0f3de1767886993e8e7b444f
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-10-17 20:24:32 +0000

    upd

--- src/Q1/lp5dash6.java
+++ src/Q1/lp5dash6.java
@@ -6,11 +6,18 @@ public class lp5dash6 {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a positive integer: ");
         int number = input.nextInt();
+        int sum = 0;
 
-        int num1 = number/100;
-        int num2 = number%10;
+        while(number > 0) {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
 
-        System.out.print("The sum of the digits is: " + num1 + num2);
+        System.out.print("The sum of the digits is: " + sum);
 
     }
 }
+/*
+ Enter a positive integer: 837
+The sum of the digits is: 18
+ */
