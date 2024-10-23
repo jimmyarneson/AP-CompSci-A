116ab580812535b6c024416698a7e91122a3b6ea
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-10-23 11:23:46 -0500

    Merge remote-tracking branch 'origin/master'

--- src/Q1/lp5_17.java
+++ src/Q1/lp5_17.java
@@ -3,6 +3,42 @@ import java.util.Scanner;
 
 public class lp5_17 {
     public static void main(String[] args) {
-        System.out.print("Enter the password");
+        String string = "fortnite67";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the password: ");
+        String guess1 = input.next();
+
+        if(string.equals(guess1)){
+            System.out.print("Correct");
+        } else{
+            System.out.print("Incorect");
+            System.out.println();
+            System.out.print("Enter the password: ");
+            String guess2 = input.next();
+            if(guess2.equals(string)){
+                System.out.print("Correct");
+            } else{
+                System.out.print("Incorect");
+                System.out.println();
+                System.out.print("Enter the password: ");
+                String guess3 = input.next();
+                if (guess3.equals(string)) {
+                    System.out.print("Correct");
+                } else {
+                    System.out.print("Incorrect");
+                    System.out.println();
+                    System.out.print("Too many trys!");
+                }
+            }
+        }
     }
 }
+/*
+Enter the password: fortnite76
+Incorect
+Enter the password: fornite87
+Incorect
+Enter the password: JimmyIsCoolReal
+Incorrect
+Too many trys!
+ */
