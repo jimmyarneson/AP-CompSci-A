af3d8fa85c62076ea48b376e7469a1694a99d421
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-10-23 19:30:24 +0000

    upd

---
+++ src/Q1/lp5_14.java
@@ -0,0 +1,11 @@
+package Q1;
+import java.util.*;
+
+public class lp5_14 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int max = 6, min = 1;
+        int lcv = 0;
+        
+    }
+}
