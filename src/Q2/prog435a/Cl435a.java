52eb1ab9bc0a900d616210bae75e3754af0ffe2b
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-12-12 11:24:40 -0600

    Merge remote-tracking branch 'origin/master'

--- src/Q2/prog435a/Cl435a.java
+++ src/Q2/prog435a/Cl435a.java
@@ -1,9 +1,23 @@
 package Q2.prog435a;
 
 public class Cl435a {
-    private String carType;
-    private int baseToll;
-    private int factor;
-    private int cost = 0;
+    private int myCt;
+    private int myG;
+    private double myC;
+    private double[] gnum = new double[8];
+    private double[] cnum = new double[6];
+    private String[] ct = new String[6];
+
+    public Cl435a(int Ct, int G){
+        myCt = Ct;
+        myG = G;
+        myC = 0.0;
+        gnum[0] = 1.35;
+        gnum[1] = 2.00;
+        gnum[2] = 2.50;
+        gnum[3] = 3.25;
+        gnum[4] = 4.10;
+
+    }
     
 }
