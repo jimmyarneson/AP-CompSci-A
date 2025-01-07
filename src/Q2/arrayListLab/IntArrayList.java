a00d0edf07a5cc652190f22950d72679edcabef5
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2025-01-07 11:24:22 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/Q2/arrayListLab/IntArrayList.java
@@ -0,0 +1,56 @@
+package Q2.arrayListLab;
+
+public class IntArrayList {
+    private int[] myArray;
+    private int mySize;
+    private int myCapacity;
+    private final int DEFAULT_SIZE = 16;
+
+    public IntArrayList() {
+        myArray = new int[DEFAULT_SIZE];
+        mySize = 0;
+        myCapacity = DEFAULT_SIZE;
+    }
+
+    private void doubleCapacity() { //DOUBLES CAPACITY OF MYARRAY
+        myCapacity = myCapacity*2;
+        int[] tempArray = new int[myCapacity];
+        for(int lcv = 0; lcv < mySize-1; lcv++) {
+            tempArray[lcv] = myArray[lcv];
+        }
+        myArray = tempArray;
+    }
+    private void halfCapacity() {
+        myCapacity = myCapacity/2;
+        int[] tempArray = new int[myCapacity];
+        for(int lcv = 0; lcv < mySize-1; lcv++) {
+            tempArray[lcv] = myArray[lcv];
+        }
+        myArray = tempArray;
+    }
+    public void add(int num) {
+        if(mySize + 1 >= myCapacity) doubleCapacity();
+
+        myArray[mySize] = num;
+        mySize++;
+        }
+    public void remove(int index){
+        for (int lcv = index +1; lcv < mySize; lcv++) {
+            myArray[lcv-1] = myArray[lcv];
+        }
+        mySize--;
+
+        if (mySize < (myCapacity * 0.25)) halfCapacity();
+    }
+    public int get(int index) {return myArray[index];}
+    public void set(int index, int num) {myArray[index] = num;}
+    public int size() {return mySize;}
+    public int[] toArray() {return myArray;}
+    public int indexOf(int num) {
+        int index = -1;
+        for (int lcv = 0; lcv < mySize; lcv++) {
+            if (num == myArray[lcv]) index = lcv;
+        }
+        return index;
+    }
+}
